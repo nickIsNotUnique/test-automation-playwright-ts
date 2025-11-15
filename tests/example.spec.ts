46e2fc8dbca8a9bd117398f6c3a72f2886230ea9
Daria Domina <nickisnotunique@gmail.com>
2025-11-15 13:34:02 +0100

    assign tags to tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,13 +1,13 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', { tag: ['@smoke', '@p0'] }, async ({ page }) => {
+test('has title', { tag: ['@smoke', '@p0', '@regression', '@desktop'] }, async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', { tag: ['@smoke'] }, async ({ page }) => {
+test('get started link', { tag: ['@smoke', '@regression', '@mobile'] }, async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
